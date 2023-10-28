49105ce99459ad1cb6b5bc38ca0c332b26e60dd4
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-28 20:21:07 +0000

    Update files

---
+++ lib.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the data
+def load(path):
+    df = pd.read_csv(path, sep=",")
+    return df
+
+# Summary the data
+def summary(data):
+    data_summary = pd.describe(data)
+    return data_summary
+
+# Visualize the data
+def visualize(data):
+    d
